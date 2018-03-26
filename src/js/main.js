ed643c9edd22179dcc36d2e3182d2a408d0deea4
Stanislav <sedoy13@yandex.ru>
2018-03-26 15:40:24 +0300

    3comit

--- src/js/main.js
+++ src/js/main.js
@@ -1,26 +1,25 @@
-var yourBudget = prompt ("Ваш бюджет?");
-var nameStore = prompt ("Название Вашего магазина?");
-var shopGoods = [];
-shopGoods[0] = prompt ("Какой тип товаров будем продавать?");
-shopGoods[1] = prompt ("Какой тип товаров будем продавать?");
-shopGoods[2] = prompt ("Какой тип товаров будем продавать?");
+var yourBudget 	= prompt ("Ваш бюджет?");
+var nameStore 	= prompt ("Название Вашего магазина?");
+var mainList 		= {};
+var open 				= 'open';
+var shopGoods 	= [];
+shopGoods[0] 		= prompt ("Какой тип товаров будем продавать?");
+shopGoods[1] 		= prompt ("Какой тип товаров будем продавать?");
+shopGoods[2] 		= prompt ("Какой тип товаров будем продавать?");
 
 mainList = {
 	yourBudget,
 	nameStore,
 	shopGoods,
-	open: 'open',
+	open: true,
 	employers: {
 		name: 'Ivan',
 		name2: 'Sergey'
 	}
-}
+};
 
 console.log(mainList);
 alert('Ваш бюджет на 1 день' + yourBudget/30);
-document.write('Ваш бюджет на 1 день' +  yourBudget/30);
-document.write('Ваш бюджет' +  yourBudget);
-document.write("Название Вашего магазина" +  nameStore);
-document.write("Какой тип товаров будем продавать" +  shopGoods);
+
 
 
