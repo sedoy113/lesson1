97df93e092ba8e6ec4da887749a07201aa4014a0
Stanislav <sedoy13@yandex.ru>
2018-03-26 16:11:06 +0300

    6comit

---
+++ light/js/main.js
@@ -0,0 +1,25 @@
+var yourBudget 	= prompt ("Ваш бюджет?");
+var nameStore 	= prompt ("Название Вашего магазина?");
+var mainList 		= {};
+var open 				= 'open';
+var shopGoods 	= [];
+shopGoods[0] 		= prompt ("Какой тип товаров будем продавать?");
+shopGoods[1] 		= prompt ("Какой тип товаров будем продавать?");
+shopGoods[2] 		= prompt ("Какой тип товаров будем продавать?");
+
+mainList = {
+	yourBudget,
+	nameStore,
+	shopGoods,
+	open: true,
+	employers: {
+		name: 'Ivan',
+		name2: 'Sergey'
+	}
+};
+
+console.log(mainList);
+alert('Ваш бюджет на 1 день' + yourBudget/30);
+
+
+
